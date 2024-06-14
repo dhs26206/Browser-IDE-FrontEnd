99a728f63237c6c92f2ee7d92908df4b84c2a9b5
Dhruv <ddkscowin@protonmail.com>
2024-06-14 21:10:40 +0530

    NavBar for Mobile Modification

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,24 +2,24 @@ import NavBar from "./NavBar";
 
 const Home=()=>{
     return(
-        <div>
+        <div className="h-screen">
          <NavBar/>
         
-        <div className="bg-[#102542] w-screen h-screen">
+        <div className="bg-[#102542] w-screen h-[88%]">
             <div className="flex justify-center items-center w-full h-full">
                     <div className="flex flex-wrap gap-5 ">
-                        <div className="w-full font-bold text-5xl  text-[#F7F0F5] text-center italic  " >                
+                        <div className="w-full font-bold px-4 text-3xl md:p-0 md:text-5xl  text-[#F7F0F5] text-center italic  " >                
                             The journey of a thousand lines of code begins with a<br></br> single problem.
                         </div>
                         <div className="text-center h-min w-full italic ">
                             Because We Don't want More Drama in our Life
                         </div>
                         <div className="flex justify-center w-full h-min gap-10 mt-5">
-                            <div className="w-52">
+                            <div className="md:w-52">
                             <button className="bg-[#4D9DE0] w-full  font-bold rounded-md border-1 border-white hover:bg-inherit">Sign Up</button>
                             </div>
-                            <div className="w-52">
-                            <button className="bg-[#F3CA40] w-full  font-bold rounded-md border-1 border-white hover:bg-inherit">See Contests</button>
+                            <div className="md:w-52">
+                            <button className="bg-[#f3ca40] w-full  font-bold rounded-md border-1 border-white hover:bg-inherit">See Contests</button>
                             </div>
                         </div>
                         <div className="flex justify-center text-center w-full mt-10 gap-2 ">
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 import Dropdown from "./DropDown";
+import Navigation from "./Navigation";
 const NavBar=()=>{
     const [click,setClick]=useState(false)
+    const [not,setNot]=useState(false);
     const divStyle = {
         background: 'rgb(16, 37, 66)',
         backgroundImage: 'linear-gradient(90deg, rgba(16,37,66,1) 6%, rgba(0,2,223,1) 28%, rgba(16,37,66,1) 100%)'
       };
+      const handlleClick=()=>{
+        setClick(prev=>!prev);
+
+      }
       const choice=["Practice","LeaderBoard","About","ContactUs","Login"]
     return(
-        <div style={divStyle} className="w-screen h-16  flex justify-between">
+        <div className="h-[12%] min-h-16 w-screen flex items-center">
+            <div className="w-screen h-full items-center flex  md:justify-center">
+            <div style={divStyle} className="w-screen h-full items-center flex justify-between">
                 <img className="w-64 h-14 pl-5" src="logo.png" alt="" />
                 <div className="text-white flex h-14 items-center hidden sm:hidden md:flex cursor-pointer"><span>Practice</span><svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" viewBox="0 0 320 512"><path fill="#ffffff" d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg></div>
                 <div className="text-white flex h-14 items-center hidden sm:hidden md:flex cursor-pointer"><span>LeaderBoard</span><svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" viewBox="0 0 320 512"><path fill="#ffffff" d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg></div>
@@ -22,13 +30,10 @@ const NavBar=()=>{
                 <div>About</div>
                 <div>Contact Us</div>
                 <div>Login</div> */}
-                <div className="flex items-center pr-5 sm:show md:hidden">
-                    <Dropdown items={choice} clickable={true}  />
-                <svg className={`${!click?'':'hidden'} h-8 w-12`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#ffffff" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
-                <svg className={`${click?'':'hidden'} h-8 w-12`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#ffffff" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
-                </div>
-            
-
+                
+            </div>
+            <Navigation choices={choice} IsOpen={handlleClick}/>
+            </div>
         </div>
     )
 }
