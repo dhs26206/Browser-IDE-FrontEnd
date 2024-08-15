f1a7d5ff46e2e226c0ac3ca31a390a1dc1a5c157
Dhruv <ddkscowin@protonmail.com>
2024-08-16 01:22:38 +0530

    Manage Contest part b

--- src/Components/Create-Contest/Manage-Contest.jsx
+++ src/Components/Create-Contest/Manage-Contest.jsx
@@ -32,35 +32,36 @@ export const ManageContest=()=>{
     return(
         <div className="pl-5 h-full w-full bg-blue-950  ">
             <div className="h-full w-full flex justify-center items-center">
-                <div className="md:w-[95%] w-[90%] md:h-[60%] ">
+                <div className="md:w-[95%] w-[90%] md:h-[70%] ">
                     <div key="gerk" className="w-full h-[10%] bg-blue-900 border-2 border-white items-center flex justify-center">
                         Yours Contest
                     </div>
-                    <div className="overflow-y-scroll h-[90%] border-2 boder-white ">
+                    <div className="overflow-y-scroll h-[90%]  boder-white ">
                     {
                         manage.map((element,index)=>{
                             // console.log(element.contest_id)
                             return(
-                                <div key={index} className={`pt-1 pl-1 w-full ${expand===index?"h-[70%]":"h-[30%]"} flex flex-col  bg-[#1d3275] hover:border-yellow-200 hover:delay-100 border-2 border-gray-800`}>
-                                    <div className={`${expand===index?"h-[40%]":"h-full"} w-full `}>
+                                <div key={index} className={`pt-1 pl-1 w-full ${expand===index?"h-[90%]":"h-[40%]"} flex flex-col  bg-[#1d3275] hover:border-yellow-200 hover:delay-100 border-2 border-gray-800`}>
+                                    <div className={`flex-grow-[4] w-full `}>
                                         <div className="w-full h-3 text-sm text-[#6e6e6e]">{element.contest_id}</div>
                                         <div className="w-full h-full pt-1 flex items-center">
                                             <div className="flex justify-between w-full">
-                                                <div className="text-2xl">{element.contest_name} </div>
+                                                <div className="text-2xl pl-4">{element.contest_name} </div>
                                                 <div className="mr-2">
                                                     <button onClick={()=>handleExpand(index)} className={`${expand===index?"bg-red-600":"bg-yellow-500"}`}>{expand===index?"Close":"View Details"}</button>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                    {expand===index&& <div className="w-full h-[60%]">
+                                    {expand===index&& <div className="w-full h-[60%] flex-grow-[9] ">
                                          <div className="w-full h-full flex flex-wrap justify-center ">
                                             <div className="w-[100%] border-t-2 border-[#aba5a5]"></div>
                                             <div className="w-full h-full flex flex-wrap items-center justify-center">
-                                                <div className="w-[90%] flex justify-between"><span>Contest ID: {element.contest_id}</span> <span>Contest Created On : {convertTime(element.created_on)} </span></div>
-                                                <div className="w-[90%] flex justify-between"><span>Start : {convertTime(element.start_time)}</span> <span>End : {convertTime(element.end_time)} </span></div>
-                                                <div className="" ><span></span> </div>
-                                                <div></div>
+                                                <div className="w-[90%] flex justify-between"><span  className="w-1/2 flex justify-start">Contest ID: {element.contest_id}</span> <span  className="w-1/2 flex justify-start">Contest Created On : {convertTime(element.created_on)} </span></div>
+                                                <div className="w-[90%] flex justify-between"><span  className="w-1/2 flex justify-start">Start : {convertTime(element.start_time)}</span> <span  className="w-1/2 flex justify-start">End : {convertTime(element.end_time)} </span></div>
+                                                <div className="w-[90%] flex justify-between"><span className="w-1/2 "> Type : {element.type} </span> <span className="w-1/2 flex justify-start"> Security : {element.security} </span></div>
+                                                
+                                                <div className="w-full flex justify-between" ><span className="w-1/2"></span> <span className="w-1/2 items-end flex justify-end gap-3 p-2"> <button className="bg-yellow-400 rounded-lg" type="button">Modify</button> <button className="bg-red-900 rounded-lg" type="button">Delete</button> </span> </div>
                                             </div>
                                          </div>
                                          </div>}
