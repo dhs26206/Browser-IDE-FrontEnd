b55a2e85d049c0534424334b87424dd24da745cd
manpreet-singh1040 <147172312+manpreet-singh1040@users.noreply.github.com>
2024-12-26 16:56:58 +0530

    ready for test deployment

--- url.js
+++ url.js
@@ -1,3 +1,4 @@
-export const url="http://localhost:3000"
+export const url="https://ashleel-backend.onrender.com"
+// export const url="http://localhost:3000"
 //  export const url="https://coback.ddks.tech"
  //module.exports={url};
