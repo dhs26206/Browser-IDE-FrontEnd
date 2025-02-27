d7284a2ed625eabef33bce5cc06bc2c57b0166aa
DDKS <77142390+dhs26206@users.noreply.github.com>
2025-02-27 13:48:52 +0000

    Minor Bug Fix

--- src/Components/Contest-Editor/Question.jsx
+++ src/Components/Contest-Editor/Question.jsx
@@ -10,7 +10,7 @@ const Question=({data})=>{
                 </div>
                 <div className="flex justify-start gap-3 pt-4">
                     <span className=" bg-fuchsia-400 text-white px-3 py-1 rounded-md">{toCamelCase(data.difficulty)}</span>
-                    <span className="text-[#95D9C3]">Accuracy: {data.acceptedSubmission/data.totalSubmission*100}</span>
+                    <span className="text-[#95D9C3]">Accuracy: {(data.acceptedSubmission/data.totalSubmission*100).toFixed(2)}%</span>
                     <span className="text-[#D1AC00]">Submission: {data.totalSubmission}</span>
                     <span className="text-[#C05746]">Max Score: {data.score}</span>
                 </div>
